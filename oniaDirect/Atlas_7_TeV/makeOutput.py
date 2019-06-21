84a2ba9f2e2b704d5a3b1c8ffa61f3b703d4e8fb
claudiocc1 <claudio@physics.ucsb.edu>
2019-06-21 16:09:20 -0700

    misc

--- oniaDirect/Atlas_7_TeV/makeOutput.py
+++ oniaDirect/Atlas_7_TeV/makeOutput.py
@@ -1,27 +1,67 @@
 #!/usr/bin/env python3
-
+#
+# Reads HEPDATA root files from Atlas 7 GeV paper
+# and outputs root files with histograms of
+# central, upper, and lower 
+#
+# The output histograms are of
+# dsigma/dpt in nb/GeV * BR(ups->mu mu)
+# integrated over abs(eta)<1.2 and rescaled
+# to 13 TeV using the parametrizations from LHCB data
+#
+# Since I was lazy, you need to set the upsilon flag by hand
+#
+# Claudio 21 June 2019
+#
 from ROOT import TH1F, TFile, gROOT, gDirectory
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-# File names for upsilon(1S)
-#HEPDatafile = "HEPData-ins1204994-v1-Table_6.root"
-#folder      = "Table 6"
-#outfile      = "Atlas_1S.root"
+# SET THIS FLAG BY HAND!!!!!!!!
+#upsilonFlag = "1S"
+#upsilonFlag = "2S"
+upsilonFlag = "3S"
 
-# File names for upsilon(2S)
-#HEPDatafile = "HEPData-ins1204994-v1-Table_7.root"
-#folder      = "Table 7"
-#outfile      = "Atlas_2S.root"
+# 1S, 2S, or 3S ???
+if upsilonFlag == "1S":
+    HEPDatafile = "HEPData-ins1204994-v1-Table_6.root"
+    folder      = "Table 6"
+    outfile     = "Atlas_1S.root"
+    LHCBFlag     = 1
+elif upsilonFlag == "2S":
+    HEPDatafile = "HEPData-ins1204994-v1-Table_7.root"
+    folder      = "Table 7"
+    outfile     = "Atlas_2S.root"
+    LHCBFlag     = 2
+elif upsilonFlag == "3S":
+    HEPDatafile = "HEPData-ins1204994-v1-Table_8.root"
+    folder      = "Table 8"
+    outfile     = "Atlas_3S.root"
+    LHCBFlag     = 3
+else:
+    print("Illegal upsilonFlag..quitting")
+    exit()
+print("processing data for upsilon ", upsilonFlag)
 
-# File names for upsilon(3S)
-HEPDatafile = "HEPData-ins1204994-v1-Table_8.root"
-folder      = "Table 8"
-outfile      = "Atlas_3S.root"
+# Function to rescale the cross-section from 7 TeV to 13 TeV
+def LHCBRescale(flag, ptrans):
+    qt = ptrans
+    if ptrans > 25:
+        qt = 25
 
-# Open the 
+    if flag == 1:
+        return 1.152 + 0.05074*qt - 0.0007649*qt*qt
+    elif flag == 2:
+        return 1.07 + 0.05387*qt - 0.001126*qt*qt
+    elif flag == 3:
+        return 1.192 + 0.0516*qt - 0.001176*qt*qt
+
+    print("Illegal call to LHCBRescale")
+    return 1.0
+        
+# Open the file
 hfile = TFile(HEPDatafile, "READ")
 hfile.cd(folder)
 
@@ -70,6 +110,15 @@ down.SetYTitle("BR(mumu) * dsigma/dpt (nb/GeV) for abs(y)<1.2")
 Ay1    = 2 * 1.2 * Ay1 / 1e6
 AyUp   = 2 * 1.2 * AyUp / 1e6
 AyDown = 2 * 1.2 * AyDown / 1e6
+
+# Rescale to 13 TeV
+for i in range(len(AyUp)):
+    thisPt = cen.GetBinCenter(i)
+    scale  = LHCBRescale(LHCBFlag, thisPt)
+    # print(thisPt, scale)
+    Ay1[i]     = scale * Ay1[i]
+    AyUp[i]    = scale * AyUp[i]
+    AyDown[i]  = scale * AyDown[i]
 
 # zero everything out
 cen.Reset("ICESM")
